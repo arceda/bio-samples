5ac82c31dd7a9d7db63e290b9eecaea5fa6f51a2
arceda <enriquefirst@gmail.com>
2019-09-22 20:47:41 -0500

    crow

---
+++ DFS/crow_search.py
@@ -0,0 +1,93 @@
+import numpy as np
+import random
+
+###########################################################################################
+############################### read data ##############################################
+instance = 'x60189_7'
+
+matrix = np.genfromtxt(instance + '/matrix_conservative.csv', delimiter=',')
+fragments = np.array([])
+
+fh = open(instance + '/frag_x60189_7.dat')
+while True:
+    line = fh.readline().replace('\n', '')
+    fragments = np.append(fragments, line)
+    if not line:
+        break
+fh.close()
+fragments = fragments[1::2]
+num_fragments = fragments.shape[0]
+
+#print(matrix.shape)
+#print(fragments.shape)
+
+###########################################################################################
+###########################################################################################
+
+ITERATIONS = 2
+N = 3
+AP = 0.92
+FL = 0.75
+P_LS = 0.45
+crows = np.zeros(shape=(N,num_fragments))
+memory = np.zeros(shape=(N,num_fragments))
+
+def show_crows(solutions):
+    print("SOLUTIONS")
+    for i in xrange(solutions.shape[0]):
+        print(solutions[i])
+        print(fitness(solutions[i]))
+
+
+def init_population():
+    for i in xrange(N):
+        crow = np.arange(num_fragments) #individual = [0, 1, 2, ...] each index is a fragment
+        np.random.shuffle(crow) #shuffle the fragment, this a ramdon solution
+        crows[i] = crow
+    return crows
+
+
+def fitness(solution):
+    #print("calculating fitness of: ", solution)
+    overlap = 0
+    for i in xrange(num_fragments - 1):
+        #print("calculating overlap of: ", int(solution[i]), int(solution[i+1]))
+        #print("overlap: ", matrix[ int(solution[i]), int(solution[i+1]) ] )
+        overlap += matrix[int(solution[i]), int(solution[i+1])] #the overload is yet calculated in matrix
+    
+    print("fitness calculated: ", overlap)
+    return overlap
+
+
+crows = init_population()
+memory = crows.copy()
+#print(crows)
+
+i=0
+while i < ITERATIONS:
+    for i in xrange(N):
+        print('CROW ', i)
+        random_crow = random.randint(0, N-1) #chose a random crow
+        r = random.random()
+        if r > AP:
+            print("the crow look up", i)
+        else:
+            print("the crow move to ramdon position", i)
+            #the crow go to a random position
+            #print('the crow go to random position', i, crows[i])
+            np.random.shuffle(crows[i])
+            #print('the crow went to random position', i, crows[i])
+            print("memory[i]: ",i,  memory[i])
+            print("crows[i]: ",i,  crows[i])
+
+        if fitness(crows[i]) > fitness(memory[i]):
+            print("the new position is better, updating memory")
+            memory[i] = crows[i].copy()
+            #print("memory[i]: ",i,  memory[i])
+            #print("crows[i]: ",i,  crows[i])
+        
+    i += 1
+
+
+show_crows(crows)
+show_crows(memory)
