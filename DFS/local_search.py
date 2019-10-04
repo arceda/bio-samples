6849c08404d3641a692a7d4b717bea7df2a4bb9d
arceda <enriquefirst@gmail.com>
2019-10-03 23:32:19 -0500

    se agrego un ciclo a PAL, con esto ya aumenta considerablementre el fitness

--- DFS/local_search.py
+++ DFS/local_search.py
@@ -3,16 +3,21 @@ import numpy as np
 def PALS(K, individual, matrix_w):
     #print("individual receive in PALS: ", individual)
     individual = individual.astype(int)
-    L = []
-    for i in range(K-2):
-        for j in range(i+1, K-1):
-            delta_c, delta_f = calculateDeltas(individual, i, j, matrix_w)
-            if delta_c < 0 or (delta_c == 0 and delta_f > 0):
-                L.append( (i, j, delta_f, delta_c) )
 
-    if len(L) > 0:
-        i, j = selectMovement(L)
-        individual = applyMovement(individual, i, j)
+    iterations = 0
+    while iterations < 300:
+        L = []
+        for i in range(K-2):
+            for j in range(i+1, K-1):
+                delta_c, delta_f = calculateDeltas(individual, i, j, matrix_w)
+                if delta_c < 0 or (delta_c == 0 and delta_f > 0):
+                    L.append( (i, j, delta_f, delta_c) )
+
+        if len(L) > 0:
+            i, j = selectMovement(L)
+            individual = applyMovement(individual, i, j)
+
+        iterations += 1
     
     return individual
 
@@ -20,7 +25,7 @@ def calculateDeltas(individual, i, j, matrix_w):
     cutoff = 30
     delta_c = delta_f = 0
     delta_f = delta_f - matrix_w[individual[i-1], individual[i]] - matrix_w[individual[j], individual[j+1]]
-    delta_f = matrix_w[individual[i-1], individual[j]] + matrix_w[individual[i], individual[j+1]]
+    delta_f = delta_f + matrix_w[individual[i-1], individual[j]] + matrix_w[individual[i], individual[j+1]]
     if matrix_w[individual[i-1], individual[i]] > cutoff:
         delta_c = delta_c + 1
     if matrix_w[individual[j], individual[j+1]] > cutoff:
@@ -74,10 +79,23 @@ def applyMovement(individual, i, j):
     return individual
 
 
+def fitness(solution, matrix):
+    #print("calculating fitness of: ", solution)
+    overlap = 0
+    for i in range(num_fragments - 1):
+        #print("calculating overlap of: ", int(solution[i]), int(solution[i+1]))
+        #print("overlap: ", matrix[ int(solution[i]), int(solution[i+1]) ] )
+        overlap += matrix[int(solution[i]), int(solution[i+1])] #the overload is yet calculated in matrix
+    
+    #print("fitness calculated: ", overlap)
+    return overlap
+
+
 if __name__ == "__main__" :
     instance = 'x60189_7'
     matrix = np.genfromtxt(instance + '/matrix_conservative.csv', delimiter=',')
     num_fragments = 68
     aleatory_solution = np.arange(num_fragments)
     np.random.shuffle(aleatory_solution)
-    print(PALS(num_fragments, aleatory_solution, matrix))
+    sol = PALS(num_fragments, aleatory_solution, matrix)
+    print(fitness(sol, matrix))
