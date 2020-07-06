a4b0ea68eca539a6653e72e335997de801d03f9f
vicente <enriquefirst@gmail.com>
2020-07-05 22:38:08 -0500

    ya se proesa todoas los atributos, queda evaluar

--- viral/alignment-free/histogram.py
+++ viral/alignment-free/histogram.py
@@ -10,6 +10,7 @@ import numpy as np
 import math
 from scipy.stats import kurtosis, skew
 from Bio import SeqIO
+import math
 
 def contrast_stretching(img):
     a = 0
@@ -59,29 +60,41 @@ for record in sequences:
 seq = data[0]
 
 ##############################################################################
-# the bases to numbers
+############################      bases to numbers      ######################
+##############################################################################
+
 feature_vector = []
 for i in range(0, len(seq)-1, 1):
     intensity = base_intensity(seq[i]+seq[i+1])    
     feature_vector.append( intensity )
 
 ##############################################################################
-# scale feature vector
+##############################################################################
+
+
+##############################################################################
+############################    scale feature vector    ######################
+##############################################################################
+# 
 feature_vector = np.array(feature_vector)
 c = feature_vector.min()
 d = feature_vector.max()
 feature_vector = feature_vector.astype(float) 
-factor = (255)/(d-c)
-feature_vector = ((feature_vector - c)*factor)   
-#print(feature_vector)
+factor = (255 - 1)/(d-c)
+feature_vector = ((feature_vector - c)*factor) + 1
+
+##############################################################################
+##############################################################################
+
+
 
 ##############################################################################
 # vector to image
 width = 70
 height = math.ceil( len(seq)/70 )
-feature_vector = np.append(feature_vector, np.zeros( ( width*height - len(feature_vector) , 1) )) # completas con ceros
+feature_vector_img = np.append(feature_vector, np.zeros( ( width*height - len(feature_vector) , 1) )) # completas con ceros
 #print(feature_vector.shape)
-img = feature_vector.reshape((height, width))
+img = feature_vector_img.reshape((height, width))
 #print(img.shape) # rows, cols
 
 img = img.astype('int8')
@@ -91,10 +104,76 @@ cv2.imwrite("gene.jpg", img)
 #cv2.waitKey()
 
 ##############################################################################
-# histogram
-hist, bins = np.histogram(feature_vector, bins=range(0,257))
+############################        histogram          #######################
+##############################################################################
+G = 256
+hist, bins = np.histogram(feature_vector, bins=range(0,G+1))
 hist = np.array(hist)
+##############################################################################
+##############################################################################
 
-print(hist.shape)
-mu = hist.dot( np.array(range(0,256)).transpose())
+
+##############################################################################
+############################         features          #######################
+##############################################################################
+i_indices = np.array(range(0, G)) # used to avoid cycles, it represent the "i" in sum
+
+p_hist = hist/feature_vector.shape[0]  # equation (3)  p(i) = h(i)/NM
+
+# equation (4)
+mu = p_hist.dot( i_indices.transpose() ) # equation (4)
 print(mu)
+#mu = 0
+#for i in range(G):
+#    mu += i*p_hist[i]
+#print(mu)
+
+# equation (5)
+variance_squared = ((i_indices - mu)*(i_indices - mu)*p_hist).sum()
+print(variance_squared)
+#variance_squared = 0
+#for i in range(G):
+#   variance_squared += (i - mu)*(i - mu)*p_hist[i]
+#print(variance_squared)
+
+# equation (6)
+standar_deviation = math.pow(variance_squared, 1/2)
+skewness = (1/math.pow(standar_deviation, 3)) * ((i_indices - mu)*(i_indices - mu)*(i_indices - mu)*p_hist).sum()
+print(skewness)
+#skewness = 0
+#for i in range(G):
+#    skewness += (i - mu)*(i - mu)*(i - mu)*p_hist[i]
+#skewness = skewness/math.pow(standar_deviation, 3)
+#print(skewness)
+
+# equation (7)
+kurtosis = (1/math.pow(standar_deviation, 4)) * ((i_indices - mu)*(i_indices - mu)*(i_indices - mu)*(i_indices - mu)*p_hist - 3).sum()
+print(kurtosis)
+#kurtosis = 0
+#for i in range(G):
+#    kurtosis += (i - mu)*(i - mu)*(i - mu)*(i - mu)*p_hist[i] - 3
+#kurtosis = kurtosis/math.pow(standar_deviation, 4)
+#print(kurtosis)
+
+# equation (8)
+energy = (p_hist*p_hist).sum()
+print(energy)
+
+# equation (9)
+entropy = 0
+for i in range(G):
+    if p_hist[i] != 0:
+        entropy += p_hist[i]*math.log( abs(p_hist[i]), 2 )
+print(entropy)
+
+##############################################################################
+##############################################################################
+
+
+
+
+
+
+
+
+
